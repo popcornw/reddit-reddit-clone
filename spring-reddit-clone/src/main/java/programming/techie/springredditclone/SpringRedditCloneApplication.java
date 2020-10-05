ad4f962c97caf478b66ed8cce6e964a6c2823b59
popcornw <46597178+popcornw@users.noreply.github.com>
2020-10-05 13:18:52 +0100

    configuration updates

--- spring-reddit-clone/src/main/java/programming/techie/springredditclone/SpringRedditCloneApplication.java
+++ spring-reddit-clone/src/main/java/programming/techie/springredditclone/SpringRedditCloneApplication.java
@@ -2,12 +2,14 @@ package programming.techie.springredditclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import programming.techie.springredditclone.config.SwaggerConfiguration;
 
 @SpringBootApplication
+
 @EnableAsync
 @Import(SwaggerConfiguration.class)
 public class SpringRedditCloneApplication {
